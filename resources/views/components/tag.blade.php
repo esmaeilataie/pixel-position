f77ad5e452b81dfb790cb1501025c5964671cf00
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-12-01 21:50:07 +0330

    use blade and tailwind techniques for laravel views

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,2 +1,13 @@
-<a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 transition-colors duration-300
-                    rounded-xl text-sm">{{$slot}}</a>
+@props(['size' => 'base'])
+
+@php
+    $classes = 'bg-white/10 hover:bg-white/25 font-bold transition-colors duration-300
+                    rounded-xl';
+    if ($size == 'base'){
+        $classes .= ' px-5 py-1 text-sm';
+    }
+    if ($size == 'small'){
+        $classes .= ' px-3 py-1 text-[0.725rem]';
+    }
+@endphp
+<a href="#" class="{{ $classes }}">{{$slot}}</a>
