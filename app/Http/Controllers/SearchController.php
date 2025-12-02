9740c434ffe7b3df3aa7eedee327151ad1699b1e
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-12-02 23:57:15 +0330

    Everything has done

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $jobs = Job::query()
+                ->with(['employer','tags'])
+                ->where('title','LIKE','%' . $request->q . '%')
+                ->get();
+        return view('results',['jobs' => $jobs]);
+    }
+}
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
     <title>Pixel Position</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body class="bg-[#060606] text-white">
+<body class="bg-[#060606] text-white pb-20">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
@@ -22,9 +22,22 @@
                 <a href="#">Salaries</a>
                 <a href="#">Companies</a>
             </div>
-            <div>
-                <a href="#">Post a Job</a>
-            </div>
+            @auth
+                <div class="flex font-bold space-x-6">
+                    <a href="/jobs/create">Post a Job</a>
+                    <form action="/logout" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="cursor-pointer">Log Out</button>
+                    </form>
+                </div>
+            @endauth
+            @guest
+                <div class="space-x-6 font-bold">
+                    <a href="/register">Sign Up</a>
+                    <a href="/login">Log In</a>
+                </div>
+            @endguest
         </nav>
         <main class="mt-10 max-w-[986px] mx-auto">
             {{$slot}}
